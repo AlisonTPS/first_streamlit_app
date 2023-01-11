7708930fa5cfcd70e4adbb015f8087832ba5f936
Alison T <122471347+AlisonTPS@users.noreply.github.com>
2023-01-11 11:47:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit. text('hard Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlt.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
