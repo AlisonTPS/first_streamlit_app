73415cdf334ab8c6228df932078ad7e5d183f499
Alison T <122471347+AlisonTPS@users.noreply.github.com>
2023-01-11 11:10:21 -0600

    Update streamlit_app.py
    Fix Missing )

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('My Parents new Healthy Diner')
 streamlit. text('Omega 3 & Blueberry Oatmeal') 
 streamlit. text('Kale, Spinach & Rocket Smoothie')
-streamlit. text('hard Boiled Free-Range Egg'
+streamlit. text('hard Boiled Free-Range Egg')
