f7ea49aa6bc749baf13cba43afdfb847f4d33ef4
Alison T <122471347+AlisonTPS@users.noreply.github.com>
2023-01-13 12:12:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 try:
   #new section to display fruityvice api response 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  IF NOT FRUIT_CHOICE:
+  if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
